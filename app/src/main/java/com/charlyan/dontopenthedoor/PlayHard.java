a0467a05e367b1e942ea4fb8afcd0321c3f28c3c
anthonyg900 <anthonyg900@yahoo.com>
2017-12-05 23:01:51 -0800

    New Popup method

--- app/src/main/java/com/charlyan/dontopenthedoor/PlayHard.java
+++ app/src/main/java/com/charlyan/dontopenthedoor/PlayHard.java
@@ -48,12 +48,14 @@ public class PlayHard extends BaseActivity {
     int templeft = 0, left = 1;
     AnimationDrawable an;
 
+    /**
     //Pop-up Window Variables
     private PopupWindow popupWindow;
     private LayoutInflater layoutInflater;
     private RelativeLayout relativeLayout;
     Runnable mRunnable;
     Handler mHandler=new Handler();
+     **/
 
 
     @Override
@@ -96,9 +98,9 @@ public class PlayHard extends BaseActivity {
         d3.setEnabled(false);
         d4.setEnabled(false);
 
+        /**
         //Creates the relative Layout to host the pop-up window
         relativeLayout = findViewById(R.id.hard_relative);
-
         //Creates handler to close pop-up window and implements the delay
         //mRunnable = new Runnable() {
            // @Override
@@ -106,6 +108,7 @@ public class PlayHard extends BaseActivity {
                 //relativeLayout.setVisibility(View.GONE);
             //}
         //};
+         **/
 
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -282,25 +285,29 @@ public class PlayHard extends BaseActivity {
 
                 if(left== 0){
 
+                    /**
                     //Creates Pop-Up window once attempts are depleted
                     layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
                     ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.popup, null);
                     popupWindow = new PopupWindow(container, 800, 800, true);
                     popupWindow.showAtLocation(relativeLayout, Gravity.NO_GRAVITY, 500, 500);
-
                     //Makes on click listener to stop pop-up window once user clicks outside of popup
                     container.setOnTouchListener(new View.OnTouchListener(){
                         @Override
                         public boolean onTouch(View view, MotionEvent motionEvent){
                             popupWindow.dismiss();
                             return true;
-
                            // container.setVisibility(View.VISIBLE);
                            // mHandler.removeCallbacks(mRunnable);
                            // mHandler.postDelayed(mRunnable, 10000);
-                        }
-                                                 }
+                        }}
                     );
+                     **/
+                    //Creates new intent when the game ends
+                    Intent pop = new Intent(getApplicationContext(), PopUp.class);
+                    startActivity(pop);
+                    //setContentView(R.layout.activity_pop_up);
+
 
                     db.open();
                     List<Scores> scoresList = db.getListOfScores();
